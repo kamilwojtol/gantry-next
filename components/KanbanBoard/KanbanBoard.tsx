777c82fa45a4f4f3077f8ead90f951454eb22430
Kamil Wojtol <kamil.wojtol97@gmail.com>
2026-08-31 21:06:25 +0200

    moving tasks logic wip

--- components/KanbanBoard/KanbanBoard.tsx
+++ components/KanbanBoard/KanbanBoard.tsx
@@ -4,85 +4,146 @@ import { useState } from "react";
 import KanbanSection from "./KanbanSection/KanbanSection";
 import KanbanTask from "./KanbanTask/KanbanTask";
 import { DragDropProvider, DragEndEvent } from "@dnd-kit/react";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import Kanban from "@/types/kanban";
 import { StatusCode } from "@/types/statusCodes";
 import Task from "@/types/task";
 
+type TasksContainer = {
+  todoTasks: Task[];
+  progressTasks: Task[];
+  doneTasks: Task[];
+};
+
+type ChangeTaskStatusParams = {
+  taskId: number;
+  newStatusCode: number;
+};
+
 export default function KanbanBoard() {
-  const [dropCategoryName, setDropCategoryName] = useState("");
+  const [taskId, setTaskId] = useState<string>("");
+  const tasks: TasksContainer = {
+    todoTasks: [],
+    progressTasks: [],
+    doneTasks: [],
+  };
   const { isPending, error, data } = useQuery<Kanban>({
     queryKey: ["kanbanBoard"],
     queryFn: () =>
       fetch("https://localhost:7224/api/kanban/1").then((res) => res.json()),
   });
 
+  const mutation = useMutation({
+    mutationFn: ({ taskId, newStatusCode }: ChangeTaskStatusParams) => {
+      return fetch(
+        `https://localhost:7224/api/kanban/1/changeTaskStatus/${taskId}?statusCode=${newStatusCode}`,
+        {
+          method: "PATCH",
+        },
+      );
+    },
+  });
+
   if (isPending) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
 
-  const mapTasks = (tasks: Task[]) => {
+  const mapTasks = () => {
     // requires optimisation
-    const todoTasks = tasks.filter((task) => {
+    tasks.todoTasks = data.tasks.filter((task) => {
       return task.status === StatusCode.TO_DO;
     });
 
-    const progressTasks = tasks.filter((task) => {
+    tasks.progressTasks = data.tasks.filter((task) => {
       return task.status === StatusCode.IN_PROGRESS;
     });
 
-    const doneTasks = tasks.filter((task) => {
+    tasks.doneTasks = data.tasks.filter((task) => {
       return task.status === StatusCode.DONE;
     });
 
     return (
       <div>
-        <KanbanSection title="To do" id="todo">
-          {todoTasks &&
-            todoTasks.map((todoTask) => (
-              <KanbanTask
-                id={todoTask.id.toString()}
-                title={todoTask.name}
-                key={todoTask.id}
-              />
-            ))}
-        </KanbanSection>
-        <KanbanSection title="In progress" id="in-progress">
-          {progressTasks &&
-            progressTasks.map((progressTask) => (
-              <KanbanTask
-                id={progressTask.id.toString()}
-                title={progressTask.name}
-                key={progressTask.id}
-              />
-            ))}
-        </KanbanSection>
-        <KanbanSection title="Done" id="done">
-          {doneTasks &&
-            doneTasks.map((doneTask) => (
-              <KanbanTask
-                id={doneTask.id.toString()}
-                title={doneTask.name}
-                key={doneTask.id}
-              />
-            ))}
-        </KanbanSection>
+        <DragDropProvider
+          onDragEnd={(event) => {
+            if (event.canceled) return;
+
+            const target = event.operation.source;
+            setTaskId(target?.element?.id.toString() ?? "");
+            console.log(target?.element);
+            mutation.mutate({
+              taskId: parseInt(taskId),
+              newStatusCode: StatusCode.DONE,
+            });
+          }}
+        >
+          <KanbanSection title="To do" id="todo">
+            {tasks.todoTasks &&
+              tasks.todoTasks.map((todoTask) => (
+                <KanbanTask
+                  id={todoTask.status}
+                  title={todoTask.name}
+                  key={todoTask.id}
+                  status={todoTask.status}
+                />
+              ))}
+          </KanbanSection>
+        </DragDropProvider>
+
+        <DragDropProvider
+          onDragEnd={(event) => {
+            if (event.canceled) return;
+
+            const target = event.operation.source;
+            setTaskId(target?.element?.id.toString() ?? "");
+            console.log(target?.element?.id);
+            mutation.mutate({
+              taskId: parseInt(taskId),
+              newStatusCode: StatusCode.DONE,
+            });
+          }}
+        >
+          <KanbanSection title="In progress" id="in-progress">
+            {tasks.progressTasks &&
+              tasks.progressTasks.map((progressTask) => (
+                <KanbanTask
+                  id={progressTask.status}
+                  title={progressTask.name}
+                  key={progressTask.id}
+                  status={progressTask.status}
+                />
+              ))}
+          </KanbanSection>
+        </DragDropProvider>
+
+        <DragDropProvider
+          onDragEnd={(event) => {
+            if (event.canceled) return;
+
+            const target = event.operation.source;
+            setTaskId(target?.element?.id.toString() ?? "");
+            console.log(target?.element?.id);
+            mutation.mutate({
+              taskId: parseInt(taskId),
+              newStatusCode: StatusCode.DONE,
+            });
+          }}
+        >
+          <KanbanSection title="Done" id="done">
+            {tasks.doneTasks &&
+              tasks.doneTasks.map((doneTask) => (
+                <KanbanTask
+                  id={doneTask.status}
+                  title={doneTask.name}
+                  key={doneTask.id}
+                  status={doneTask.status}
+                />
+              ))}
+          </KanbanSection>
+        </DragDropProvider>
       </div>
     );
   };
 
-  return (
-    <div className="flex border border-black">
-      <DragDropProvider
-        onDragEnd={(event) => {
-          if (event.canceled) return;
-
-          const target = event.operation.target;
-          setDropCategoryName(target?.id.toString() ?? "");
-        }}
-      >
-        {mapTasks(data.tasks)}
-      </DragDropProvider>
-    </div>
-  );
+  return <div className="flex border border-black">{mapTasks()}</div>;
 }
--- components/KanbanBoard/KanbanTask/KanbanTask.tsx
+++ components/KanbanBoard/KanbanTask/KanbanTask.tsx
@@ -1,17 +1,19 @@
+import { StatusCode } from "@/types/statusCodes";
 import { useDraggable } from "@dnd-kit/react";
 
 type KanbanTaskProps = {
   title: string;
-  id: string;
+  id: number;
+  status: StatusCode;
 };
 
-export default function KanbanTask({ title, id }: KanbanTaskProps) {
+export default function KanbanTask({ title, id, status }: KanbanTaskProps) {
   const { ref } = useDraggable({
     id: id,
   });
 
   return (
-    <div ref={ref}>
+    <div ref={ref} id={id.toString()}>
       <h3>{title}</h3>
     </div>
   );
