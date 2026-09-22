ab51deb44d29197cdce0c6208beded4aac221b4e
Kamil Wojtol <kamil.wojtol97@gmail.com>
2026-09-22 20:11:29 +0200

    addding task wip

--- components/CreateTask/CreateTask.tsx
+++ components/CreateTask/CreateTask.tsx
@@ -4,6 +4,9 @@ import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { Button, TextField, Divider } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Dayjs } from "dayjs";
+import { useMutation } from "@tanstack/react-query";
+import Task from "@/types/task";
+import { useKanban } from "@/store/useKanban";
 
 type CreateTaskProps = {
   title: string;
@@ -12,6 +15,7 @@ type CreateTaskProps = {
 type Inputs = {
   taskName: string;
   taskDescription: string;
+  taskAuthor: string;
   deadline: Dayjs | null;
 };
 
@@ -22,7 +26,25 @@ export default function CreateTask({ title }: CreateTaskProps) {
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) =>
+    createTaskMutation.mutate(data);
+
+  const getKanbanId = useKanban((state) => state.kanban?.id ?? 1);
+
+  const createTaskMutation = useMutation({
+    mutationFn: (task) => {
+      return fetch(
+        `http://localhost:5142/api/kanban/${getKanbanId}/createTask`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(task),
+        },
+      );
+    },
+  });
 
   return (
     <div>
@@ -63,6 +85,19 @@ export default function CreateTask({ title }: CreateTaskProps) {
               {...register("taskDescription")}
               id="kanban-description"
               error={Boolean(errors.taskDescription)}
+              className="w-75"
+            />
+          </div>
+
+          <div className="flex items-center gap-2 my-2">
+            <label htmlFor="kanban-author" className="w-25">
+              Author:
+            </label>
+            <TextField
+              defaultValue="Author"
+              {...register("taskAuthor")}
+              id="kanban-author"
+              error={Boolean(errors.taskAuthor)}
               className="w-75"
             />
           </div>
